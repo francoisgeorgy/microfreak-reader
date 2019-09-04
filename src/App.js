204e5ad2f7c42ac2e8a8e879e2292bad11a836dd
François Georgy <francois.georgy@gmail.com>
2019-09-04 14:56:17 +0200

    main grid layout

--- src/App.js
+++ src/App.js
@@ -49,28 +49,49 @@ class App extends Component {
                     </div>
                     <div>
                         <div className="controls">
-                            <div>Glide</div><div className="value">{P[5]}</div>
-                            <div>Type</div><div className="value">{P[9]}</div>
-                            <div>Wave</div><div className="value">{P[10]}</div>
-                            <div>Timbre</div><div className="value">{P[12]}</div>
-                            <div>Shape</div><div className="value">{P[13]}</div>
-                            <div>Cutoff</div><div className="value">{P[23]}</div>
-                            <div>Resonance</div><div className="value">{P[83]}</div>
-                            <div>Cycling Env Rise</div><div className="value">{P[102]}</div>
-                            <div>Cycling Env Fall</div><div className="value">{P[103]}</div>
-                            <div>Cycling Env Hold</div><div className="value">{P[28]}</div>
-                            <div>Cycling Env Amount</div><div className="value">{P[24]}</div>
-                            <div>ARP/SEQ Rate (free)</div><div className="value">{P[91]}</div>
-                            <div>ARP/SEQ Rate (sync)</div><div className="value">{P[92]}</div>
-                            <div>LFO Rate (free)</div><div className="value">{P[93]}</div>
-                            <div>LFO Rate (sync)</div><div className="value">{P[94]}</div>
-                            <div>Envelope Attack</div><div className="value">{P[105]}</div>
-                            <div>Envelope Decay</div><div className="value">{P[106]}</div>
-                            <div>Envelope Sustain</div><div className="value">{P[29]}</div>
-                            <div>Keyboard Hold button (toggle)</div><div className="value">{P[64]}</div>
-                            <div>Keyboard Spice</div><div className="value">{P[2]}</div>
-                            <div>Keyboard Pitch Bend</div><div className="value">Pitchbend</div>
-                            <div>Keyboard Pressure</div><div className="value">Aftertouch</div>
+                            <div className="group oscillator">
+                                <h3>Oscillator</h3>
+                                <div>Type</div><div className="value">{P[9]}</div>
+                                <div>Wave</div><div className="value">{P[10]}</div>
+                                <div>Timbre</div><div className="value">{P[12]}</div>
+                                <div>Shape</div><div className="value">{P[13]}</div>
+                            </div>
+                            <div className="group filter">
+                                <h3>Filter</h3>
+                                <div>Cutoff</div><div className="value">{P[23]}</div>
+                                <div>Resonance</div><div className="value">{P[83]}</div>
+                            </div>
+                            <div className="group cycling-env">
+                                <h3>Cycling envelope</h3>
+                                <div>Cycling Env Rise</div><div className="value">{P[102]}</div>
+                                <div>Cycling Env Fall</div><div className="value">{P[103]}</div>
+                                <div>Cycling Env Hold</div><div className="value">{P[28]}</div>
+                                <div>Cycling Env Amount</div><div className="value">{P[24]}</div>
+                            </div>
+                            <div className="group arp-seq">
+                                <h3>ARP/SEQ</h3>
+                                <div>ARP/SEQ Rate (free)</div><div className="value">{P[91]}</div>
+                                <div>ARP/SEQ Rate (sync)</div><div className="value">{P[92]}</div>
+                            </div>
+                            <div className="group lfo">
+                                <h3>LFO</h3>
+                                <div>LFO Rate (free)</div><div className="value">{P[93]}</div>
+                                <div>LFO Rate (sync)</div><div className="value">{P[94]}</div>
+                            </div>
+                            <div className="group env">
+                                <h3>Envelope</h3>
+                                <div>Envelope Attack</div><div className="value">{P[105]}</div>
+                                <div>Envelope Decay</div><div className="value">{P[106]}</div>
+                                <div>Envelope Sustain</div><div className="value">{P[29]}</div>
+                            </div>
+                            <div className="group keyboard">
+                                <h3>Keyboard</h3>
+                                <div>Glide</div><div className="value">{P[5]}</div>
+                                <div>Keyboard Hold button (toggle)</div><div className="value">{P[64]}</div>
+                                <div>Keyboard Spice</div><div className="value">{P[2]}</div>
+                                <div>Keyboard Pitch Bend</div><div className="value">Pitchbend</div>
+                                <div>Keyboard Pressure</div><div className="value">Aftertouch</div>
+                            </div>
                         </div>
                     </div>
                 </div>
